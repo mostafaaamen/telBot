f66b28778dee8672d4bc9ebdf9a82383f2ee6715
mostafaaamen <88153348+mostafaaamen@users.noreply.github.com>
2023-11-16 14:02:47 +0200

    Update index.js
    add /link

--- index.js
+++ index.js
@@ -4,20 +4,103 @@ const express = require("express")
 const app = express()
 const bot = new Telegraf(process.env.TOKEN)
 function hasLinks(text) {
+    let res = []
+    const linkRegex = /(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?/gi;
     const urlPattern = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?(\?[\w-]+=[\w-%&]*)?$/i;
-    return urlPattern.test(text);
+    res.push(urlPattern.test(text))
+    res.push(linkRegex.test(text))
+    return res
 }
+let html = `
+This Bot Allow To Delete New Chat Have alink
+
+/link   : Show All Links You Need It
+`
+bot.start((ctx) => {
+    const htmlText = '<b>Hello</b>, <i>Telegram</i>!';
+    ctx.reply(html, { parse_mode: 'HTML' });
+});
+
+
+bot.command("link", (ctx) => {
+    let html = `
+    ----------------------------------
+        <a href="http://books.sadat.online/">المنصة</a>
+
+        <a href="http://stu.usc.edu.eg"> ابن الهيثم </a>
+
+        <a href="https://t.me/DwratTrbiaAskria">  التربية العسكرية </a>
+
+        <a href="https://forms.gle/h2QPrJ5juFCDvXnd8">تسجيل مهارات التوظيف</a>
+
+        <a href="https://maps.app.goo.gl/UVBJix35uXkf7RwR6">مكان مهارات التوظيف علي جوجل </a>
+
+    ----------------------------------
+    الكتب و المذكرات
+
+<b>الفرقة الرابعة</b> :
+
+    <b>كتب الترم الاول</b>    : <a href="https://t.me/AzmosBooks/11301">اضعط</a>
+
+    <b>مذكرات الترم الاول</b> : <a href="https://t.me/AzmosBooks/11455">اضعط</a>
+
+<b>الثالثة </b> :
+    
+    <b>كتب الترم الاول</b>    : <a href="https://t.me/AzmosBooks/4903">اضعط</a>
+    
+    <b>مذكرات الترم الاول</b>: <a href="https://t.me/AzmosBooks/5319">اضعط</a>
+
+
+    <b>كتب الترم الثاني</b>    : <a href="https://t.me/AzmosBooks/7163">اضعط</a>
+    
+    <b>مذكرات الترم الثاني</b>: <a href="https://t.me/AzmosBooks/7535">اضعط</a>
+
+<b>الثانية </b> :
+
+    <b>كتب الترم الاول</b>    : <a href="https://t.me/AzmosBooks/72">اضعط</a>
+
+    
+    <b>كتب الترم الثاني</b>    : <a href="https://t.me/AzmosBooks/1469">اضعط</a>
+    
+
+    ────────────────────
+   <b>Created By : </b> @DevPenetration
+    ────────────────────
+    this remover after 10s
+    ────────────────────`
+    const text = ctx.message.text;
+    console.log(ctx.message.from.username)
+    const chatId = ctx.message.chat.id; // Get the chat ID
+    const messageId = ctx.message.message_id;
+    ctx.reply(html, { parse_mode: "HTML", disable_web_page_preview: true })
+    setTimeout(() => { 
+        ctx.telegram.deleteMessage(chatId, messageId)
+        ctx.telegram.deleteMessage(chatId, messageId + 1)
+    },10000)
+})
+bot.command("test", (ctx) => {
+    const centeredText =
+        'Hello,\n\n' +
+        'Here is a way to create the illusion of centered text on Telegram.\n' +
+        '────────────────────\n\n' + // Create a horizontal line for visual separation
+        'Centered Text\n\n' +
+        '────────────────────\n\n' + // Create another horizontal line
+        'This text appears centered!';
+
+    ctx.reply(centeredText);
+})
+
 bot.hears(/.*/,async (ctx) => {
     const text = ctx.message.text;
-    const textLength = text.length;
     let userName = ctx.message.from.username
     console.log(ctx.message.from.username)
     let permesionsUser = ["DevPenetration" ]
     console.log("permesions:  ", permesionsUser.includes(userName))
+    console.log(hasLinks(text).includes(true))
     if (permesionsUser.includes(userName)) {
         return
     } else { 
-        if (hasLinks(text)) {
+        if (hasLinks(text).includes(true)) {
             try {
                 const chatId = ctx.message.chat.id; // Get the chat ID
                 const messageId = ctx.message.message_id; // Get the ID of the message to delete
@@ -36,7 +119,7 @@ can you send to
 @DevPenetration or @DevAtacker
                     ----------------------------------------------
 
-System Created By
+System Created By <a href="te.me/azmos-book">Group</a>
              @DevPenetration
 massage delete after 10s
     `)
@@ -56,3 +139,6 @@ app.get("/", (req, res) => {
 })
 let PORT=process.env.PORT||3002
 app.listen(PORT,()=>console.log(`server running on ${PORT}`))
+
+
+
